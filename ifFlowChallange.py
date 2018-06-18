4fb2dd511de94abe8e24bb89c4b34c55a87ac502
Mallika Sinha <msinha034@gmail.com>
2018-06-18 08:53:30 +0530

    ifFlowChallange add

---
+++ ifFlowChallange.py
@@ -0,0 +1,7 @@
+name = input("enter your name")
+age = int(input(" enter your age"))
+
+if (age > 18) and (age <31):
+    print("Welcome, to the holiday club")
+else:
+    print("Sorry ,you can't go to holiday")
